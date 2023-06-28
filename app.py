94bda31e938b5baef8cc9427fca47c8c9c9d3752
MariaEuC <castanoh.mariae@gmail.com>
2023-06-27 19:01:53 -0500

    Enemies added

--- app.py
+++ app.py
@@ -9,11 +9,13 @@ pygame.init()
 screen = pygame.display.set_mode((900, 600))
 clock = pygame.time.Clock()
 score = 0
+enemy_score = 0
 bottle_list = pygame.sprite.Group()
+enemy_list = pygame.sprite.Group()
 all_sprite_list = pygame.sprite.Group()
 
 
-# Se crea los sprites de las botellas
+# Se crean los sprites de las botellas
 class Bottle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -23,8 +25,17 @@ class Bottle(pygame.sprite.Sprite):
         # Se toman la posicion
         self.rect = self.image.get_rect()
 
+# Se crean los sprites de los enemigos
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("basura.png").convert()
+        # Se quita el color negro del fondo
+        self.image.set_colorkey(Colores.BLACK)
+        # Se toman la posicion
+        self.rect = self.image.get_rect()
 
-# Se crea los sprites de las botellas
+# Se crean los sprites del reciclador
 class Recycler(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -38,7 +49,7 @@ recycler = Recycler()
 
 all_sprite_list.add(recycler)
 
-# Crear menu
+
 for i in range(20):
     bottle = Bottle()
     bottle.rect.x = random.randrange(800)
@@ -47,9 +58,18 @@ for i in range(20):
     bottle_list.add(bottle)
     all_sprite_list.add(bottle)
 
+for i in range(5):
+    enemy = Enemy()
+    enemy.rect.x = random.randrange(900)
+    enemy.rect.y = random.randrange(600)
+
+    enemy_list.add(enemy)
+    all_sprite_list.add(enemy)
+
 
 def start_the_game():
     score = 0
+    enemy_score = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -70,12 +90,23 @@ def start_the_game():
 
         # Se detectan las colisiones
         bottle_collide_list = pygame.sprite.spritecollide(recycler, bottle_list, True)
+        # Se detectan las colisiones
+        enemy_collide_list = pygame.sprite.spritecollide(recycler, enemy_list, True)
+
 
         # Para guardar el marcador
         for botlle in bottle_collide_list:
             score += 1
+        for enemy in enemy_collide_list:
+            enemy_score += 1
 
         # se acaba el juego
+        if enemy_score > 0:
+            screen.fill(Colores.WHITE)
+            font = pygame.font.SysFont("arial", 24)
+            text = font.render(
+                "Reciclaste una basura enemigo perdiste!!!", True, Colores.BLACK)
+            screen.blit(text, (200, 250))
         if score == 20:
             screen.fill(Colores.WHITE)
             font = pygame.font.SysFont("arial", 24)
